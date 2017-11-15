035e5bea6a9bc313ed0f13c4e6d6a97f97bf4cc0
juliabrosova <33289336+juliabrosova@users.noreply.github.com>
2017-11-15 19:47:26 +0500

    first commit

---
+++ IdeaProjects/Task 2.2/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Owl[] owls =new Owl[4];
+
+
+        owls [0]= new Owl ();
+        owls [0].sleep();
+
+        owls [1]=new Owl();
+        owls [1].sleep();
+
+        owls [2]=new Owl();
+        owls [2].hunt();
+
+        owls [3]=new Owl();
+        owls [3].eatPrey();
+
+
+
+    }
+}
---
+++ IdeaProjects/Task 2.2/src/com/company/Owl.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Owl {
+    private String name;
+    private String breed;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getBreed() {
+
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+
+        this.breed = breed;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age;
+    }
+
+    public void hunt() {
+        System.out.println("Сова охотится");
+
+    }
+
+    public void eatPrey() {
+        System.out.println("Сова пожирает добычу");
+
+    }
+
+    public void sleep() {
+        System.out.println("Сова спит");
+
+    }
+
+}
+
